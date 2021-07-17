02d46a5506f031d26dce6358c44a7a8d273258db
Thienle <wavelets11@gmail.com>
2021-07-17 20:03:28 +0700

    Add files via upload

---
+++ animal_app.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 17 19:18:00 2021
+
+@author: thienle
+"""
+import cv2
+import numpy as np
+import streamlit as st
+import tensorflow as tf
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
+
+model = tf.keras.models.load_model("D:\OneDrive\streamlitprojects\\animals\saved_model\mdl_wt.hdf5")
+### load file
+uploaded_file = st.file_uploader("Choose a image file", type="jpg")
+
+map_dict = {0: 'dog',
+            1: 'horse',
+            2: 'elephant',
+            3: 'cow',
+            4: 'cat',
+            5: 'chicken',
+            6: 'butterfly',
+            7: 'sheep',
+            8: 'squirrel',
+            9: 'spider'}
+
+
+if uploaded_file is not None:
+    # Convert the file to an opencv image.
+    file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
+    opencv_image = cv2.imdecode(file_bytes, 1)
+    opencv_image = cv2.cvtColor(opencv_image, cv2.COLOR_BGR2RGB)
+    resized = cv2.resize(opencv_image,(224,224))
+    # Now do something with the image! For example, let's display it:
+    st.image(opencv_image, channels="RGB")
+
+    resized = mobilenet_v2_preprocess_input(resized)
+    img_reshape = resized[np.newaxis,...]
+
+    Genrate_pred = st.button("Generate Prediction")    
+    if Genrate_pred:
+        prediction = model.predict(img_reshape).argmax()
+        st.title("Predicted Label for the image is {}".format(map_dict [prediction]))
