e330a8cf2003159ff0e1aa66b402066df43bcd7f
Thienle <wavelets11@gmail.com>
2021-07-17 21:35:23 +0700

    Update animal_app.py

--- animal_app.py
+++ animal_app.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2,preprocess_input as mobilenet_v2_preprocess_input
 
-model = tf.keras.models.load_model("D:\OneDrive\streamlitprojects\\animals\saved_model\mdl_wt.hdf5")
+model = tf.keras.models.load_model("saved_model/mdl_wt.hdf5")
 ### load file
 uploaded_file = st.file_uploader("Choose a image file", type="jpg")
 
